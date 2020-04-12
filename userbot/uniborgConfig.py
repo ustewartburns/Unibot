264fbead9547d08180bcb24b48064ea8e5351f7b
༺αиυвιѕ༻ <54077356+Dark-Princ3@users.noreply.github.com>
2020-04-12 19:30:20 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -36,7 +36,7 @@ if ENV:
         #
         NO_SONGS = bool(os.environ.get("NO_SONGS", False))
         #
-        PMPERMIT_MENU = bool(os.environ.get("NO_SONGS", False))
+        PMPERMIT_MENU = bool(os.environ.get("PMPERMIT_MENU", False))
         #
         # DO NOT EDIT BELOW THIS LINE IF YOU DO NOT KNOW WHAT YOU ARE DOING
         # TG API limit. A message can have maximum 4096 characters!
