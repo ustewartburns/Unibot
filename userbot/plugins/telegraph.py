d2bfb1383d71f40e89690717cffe8081900ac4dc
ustewartburns <63700187+ustewartburns@users.noreply.github.com>
2020-04-18 13:29:09 +0700

    Update telegraph.py

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -1,7 +1,7 @@
-"""@telegraph Utilities
-Available Commands:
-.telegraph media as reply to a media
-.telegraph text as reply to a large text"""
+"""@telegraph Utilitas
+Perintah yang tersedia:
+.telegraph media saat balas ke media
+.telegraph teks saat balas teks yang panjang"""
 from telethon import events
 import os
 from PIL import Image
@@ -14,18 +14,18 @@ r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
 auth_url = r["auth_url"]
 
 
-@borg.on(admin_cmd("telegraph (media|text) ?(.*)"))
+@borg.on(admin_cmd("telegraph (media|teks) ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
     if Config.PRIVATE_GROUP_BOT_API_ID is None:
-        await event.edit("Please set the required environment variable `PRIVATE_GROUP_BOT_API_ID` for this plugin to work")
+        await event.edit("Silakan atur yang diperlukan environment variable `PRIVATE_GROUP_BOT_API_ID` agar plugin ini berfungsi")
         return
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     await borg.send_message(
         Config.PRIVATE_GROUP_BOT_API_ID,
-        "Created New Telegraph account {} for the current session. \n**Do not give this url to anyone, even if they say they are from Telegram!**".format(auth_url)
+        "Membuat akun Telegraph Baru {} untuk sesi saat ini. \n**Jangan berikan url ini kepada siapa pun, bahkan jika mereka mengatakan itu dari Telegram!**".format(auth_url)
     )
     optional_title = event.pattern_match.group(2)
     if event.reply_to_msg_id:
@@ -39,7 +39,7 @@ async def _(event):
             )
             end = datetime.now()
             ms = (end - start).seconds
-            await event.edit("Downloaded to {} in {} seconds.".format(downloaded_file_name, ms))
+            await event.edit("Diunduh dalam {} waktu {} detik.".format(downloaded_file_name, ms))
             if downloaded_file_name.endswith((".webp")):
                 resize_image(downloaded_file_name)
             try:
@@ -52,7 +52,7 @@ async def _(event):
                 end = datetime.now()
                 ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
-                await event.edit("Uploaded to https://telegra.ph{} in {} seconds.".format(media_urls[0], (ms + ms_two)), link_preview=True)
+                await event.edit("Diunggah ke telegra.ph{} dalam {} detik.".format(media_urls[0], (ms + ms_two)), link_preview=True)
         elif input_str == "text":
             user_object = await borg.get_entity(r_message.from_id)
             title_of_page = user_object.first_name # + " " + user_object.last_name
@@ -80,9 +80,9 @@ async def _(event):
             )
             end = datetime.now()
             ms = (end - start).seconds
-            await event.edit("Pasted to https://telegra.ph/{} in {} seconds.".format(response["path"], ms), link_preview=True)
+            await event.edit("Diunggah ke ke https://telegra.ph/{} dalam {} detik.".format(response["path"], ms), link_preview=True)
     else:
-        await event.edit("Reply to a message to get a permanent telegra.ph link. (Inspired by @ControllerBot)")
+        await event.edit("Balas pesan Text atau Media untuk mendapatkan tautan telegra.ph secarapermanen. (Terinspirasi oleh @ControllerBot)")
 
 
 def resize_image(image):
